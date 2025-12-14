ee453b9388837cf3a59be2e6a870aa2bd18f80a9
ShanayaRaval <dhaval.raval12@gmail.com>
2025-12-14 17:15:03 +0530

    Add files via upload

--- Homework.py
+++ Homework.py
@@ -1,11 +1,12 @@
-print("Hello! I'm AI Bot. What's your name?")
-name=input
-print(f"Nice to meet you{name}")
-print("How are you feelig today?:good/bad")
-if mood=="good":
-    print("I'm glad to hear that.")
-elif mood =="bad":
-    print("I'm so sorry to hear that.")
+print("Hello! I am AI Bot. What's your name? ")
+name = input ()
+print(f'Nice to meet you,{name}')
+print('How are you feeling today? good/bad')
+mood = input().lower()
+if mood=='good':
+    print('I am glad to hear that.')
+elif mood=='bad':
+    print("I am sorry to hear that.")
 else :
-    print("I see it is sometimes hard to put feelings into words.")
-print(f"It was nice catting with you, {name}")
+    print("I see it is sometimes hard to put feelings into words")
+print(f"It was nice chatting with you {name}")
