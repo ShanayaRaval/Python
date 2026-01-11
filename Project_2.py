6f68a97248e1999fdb2547a2e8069cd1880bda6c
ShanayaRaval <dhaval.raval12@gmail.com>
2026-01-11 18:22:47 +0530

    Add files via upload

---
+++ Project_2.py
@@ -0,0 +1,13 @@
+frnd1 = "7 June 2015"
+frnd2 = "8 September 2014"
+frnd3 = "3 March 2015"
+frnd4 = "26 November 2015"
+frnd5 = "24 October 2014"
+
+print("Raj has 4 Friends.")
+
+print("\nRadha's Birthday is on:", frnd1)
+print("Mohit's Birthday is on:", frnd2)
+print("Prisha's Birthday is on:", frnd3)
+print("Rohan's Birthday is on:", frnd4)
+print("Aviral's Birthday is on:", frnd5)
