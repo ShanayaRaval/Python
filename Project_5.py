059d443976f7075bc77425369211409f2c927752
ShanayaRaval <dhaval.raval12@gmail.com>
2026-01-18 17:27:27 +0530

    Add files via upload

---
+++ Project_5.py
@@ -0,0 +1,7 @@
+weather = str(input("Please enter today's weather (sunny/chilly):"))
+sunny = input
+
+if weather == 'sunny': 
+   print("Wear light clothes.") 
+else:
+   print("Don't wear light clothes.")
