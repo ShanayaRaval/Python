f65425292f86e682a37f815bdf559e1e13db4e38
ShanayaRaval <dhaval.raval12@gmail.com>
2026-01-18 16:38:14 +0530

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a number: "))
+if i < 15:
+    print("This no. is smaller than 15.")
+    print("This is in if block.")
+else:
+    print("This no. is greater than 15.")
+    print("This is in else block.")
+print("This is neither in if nor else block.")
