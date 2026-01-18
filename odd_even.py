efb824e21b94f84b943afa317a3fc5152e0aeca1
ShanayaRaval <dhaval.raval12@gmail.com>
2026-01-18 16:37:52 +0530

    Add files via upload

---
+++ odd_even.py
@@ -0,0 +1,7 @@
+number = int(input("Please enter number to be checked:"))
+print("Number to be checked:", number)
+
+if number % 2 == 0:
+    print("This number is even.")
+else:
+    print("This number is odd.")
