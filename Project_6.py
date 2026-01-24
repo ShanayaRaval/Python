3366ffb3e4e65234bcff1ec66f0f8e49a1be6504
ShanayaRaval <dhaval.raval12@gmail.com>
2026-01-24 17:28:46 +0530

    Add files via upload

---
+++ Project_6.py
@@ -0,0 +1,82 @@
+a = input("Enter any letter/number/symbol:")
+
+if a =='a':
+    print("This is an alphabet.")
+
+elif a =='b':
+    print("This is an alphabet.")
+
+elif a =='c':
+    print("This is an alphabet.")
+
+elif a =='d':
+    print("This is an alphabet.")
+
+elif a =='e':
+    print("This is an alphabet.")
+
+elif a =='f':
+    print("This is an alphabet.")
+
+elif a =='g':
+    print("This is an alphabet.")
+
+elif a =='h':
+    print("This is an alphabet.")
+
+elif a =='i':
+    print("This is an alphabet.")
+
+elif a =='j':
+    print("This is an alphabet.")
+
+elif a =='k':
+    print("This is an alphabet.")
+
+elif a =='l':
+    print("This is an alphabet.")
+
+elif a =='m':
+    print("This is an alphabet.")
+
+elif a =='n':
+    print("This is an alphabet.")
+
+elif a =='o':
+    print("This is an alphabet.")
+
+elif a =='p':
+    print("This is an alphabet.")
+
+elif a =='q':
+    print("This is an alphabet.")
+
+elif a =='r':
+    print("This is an alphabet.")
+
+elif a =='s':
+    print("This is an alphabet.")
+
+elif a =='t':
+    print("This is an alphabet.")
+
+elif a =='u':
+    print("This is an alphabet.")
+
+elif a =='v':
+    print("This is an alphabet.")
+
+elif a =='w':
+    print("This is an alphabet.")
+
+elif a =='x':
+    print("This is an alphabet.")
+
+elif a =='y':
+    print("This is an alphabet.")
+
+elif a =='z':
+    print("This is an alphabet.")
+
+else:
+    print("This isn't an alphabet.")
