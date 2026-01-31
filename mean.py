f8541b8eb6bda47f8fc6febfdf466ba9f751dbe3
ShanayaRaval <dhaval.raval12@gmail.com>
2026-01-31 16:48:59 +0530

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,13 @@
+mean1 = 38
+wrong_num = 36
+correct_num = 56
+tot_num = 40
+
+sum = mean1 * tot_num
+print("The sum of 40 numbers is:", sum)
+
+num2 = sum - (wrong_num - correct_num)
+print("sum - (wrong_num - correct_num) is:", num2)
+
+mean2 = num2/tot_num
+print(mean2)
---
+++ num.py
@@ -0,0 +1,10 @@
+print("Enter a number(numerator):")
+num = int(input())
+print("Enter a number(denominator):")
+numd = int(input())
+
+if num%numd == 0:
+    print("\n" + str(num) + " is divisible by " + str(numd))
+
+else:
+     print("\n" + str(num) + " is not divisible by " + str(numd))
