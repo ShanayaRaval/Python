806ce61538e7f2d331291e76d94e88c3d68d98a5
ShanayaRaval <dhaval.raval12@gmail.com>
2026-02-15 12:31:31 +0530

    Add files via upload

---
+++ Project_10.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number:"))
+pwr = int(input("Enter the power:"))
+
+i = pwr
+
+ans = num ** i
+
+print("The Answer is: ", ans)
---
+++ Project_9.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age:"))
+
+if age >= 10:
+    if age <=20:
+        print("You are allowed!")
+
+elif age < 10:
+    if age > 20:
+        print("You are not allowed!")
+
+else:
+    print("Invalid Input!")
