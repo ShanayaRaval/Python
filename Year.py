24ff903a1cf4696b5ed37be95de9f3d7f3e41eed
ShanayaRaval <dhaval.raval12@gmail.com>
2026-04-04 16:57:08 +0530

    Add files via upload

---
+++ Year.py
@@ -0,0 +1,61 @@
+import calendar 
+
+year = 2026
+month = 1
+
+print(calendar.month(year, month))
+
+year = 2026
+month = 2
+
+print(calendar.month(year, month))
+
+year = 2026
+month = 3
+
+print(calendar.month(year, month))
+
+year = 2026
+month = 4
+
+print(calendar.month(year, month))
+
+year = 2026
+month = 5
+
+print(calendar.month(year, month))
+
+year = 2026
+month = 6
+
+print(calendar.month(year, month))
+
+year = 2026
+month = 7
+
+print(calendar.month(year, month))
+
+year = 2026
+month = 8
+
+print(calendar.month(year, month))
+
+year = 2026
+month = 9
+
+print(calendar.month(year, month))
+
+year = 2026
+month = 10
+
+print(calendar.month(year, month))
+
+year = 2026
+month = 11
+
+print(calendar.month(year, month))
+
+year = 2026
+month = 12
+
+print(calendar.month(year, month))
