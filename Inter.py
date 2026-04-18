08a4f88a400cf48f5b53d11a542e525b5d337593
ShanayaRaval <dhaval.raval12@gmail.com>
2026-04-18 16:43:53 +0530

    Add files via upload

---
+++ Inter.py
@@ -0,0 +1,10 @@
+setx = {'green', 'blue'}
+sety = {'yellow', 'blue'}
+
+print("Original set elements: ")
+print(setx)
+print(sety)
+
+print("\nIntersection of the two sets: ")
+setz = setx.intersection(sety)
+print(setz)
